440f4998edd0ebb7ceb6b4602275fb61f5bfb343
Ronald Mora <ronaldmorar22@gmail.com>
2026-09-06 01:56:35 -0600

    Añade funcion InciarConexion a clase Servidor

--- Servidor.cs
+++ Servidor.cs
@@ -1,10 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 
+//https://youtu.be/GWkdPC74css
+//https://youtu.be/IyiDYFfDLyc
+//https://youtu.be/TAGoid4u6PY
+//https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/sockets/tcp-classes
 namespace Monopoly.App
 {
-    internal class Servidor
+    public class Servidor
     {
+        private TcpListener listener;
+        private Banco banco;
+        private bool prendido;
+        private int jugadorId;
+        private List<ClienteConectado> clientes;
+
+
+        public Servidor(int puerto, Banco banco)
+        {
+            listener = new TcpListener(IPAddress.Any, puerto);
+            clientes = new List<ClienteConectado>();
+            this.banco = banco;
+            jugadorId = 1;
+        }
+//https://learn.microsoft.com/es-es/dotnet/csharp/asynchronous-programming/
+        public async Task IniciarConexionAsync()
+        {
+            listener.Start();
+            prendido = true;
+            while (prendido)
+            {
+                TcpClient cliente = await listener.AcceptTcpClientAsync();
+                _ = EscucharClienteAsync(cliente);
+            }
+        }
+
+        public async Task EscucharClienteAsync(TcpClient sCliente)
+        {
+            
+        }
+
+
+    }
+
+    public class ClienteConectado
+    {
+        public int IdJugador { get; set; }
+        public TcpClient Socket { get; }
+        public StreamReader Lector { get; }
+        public StreamWriter Escritor { get; }
+        public ClienteConectado(TcpClient socket)
+        {
+            Socket = socket;
+            var stream = socket.GetStream();
+            Lector = new StreamReader(stream, Encoding.UTF8);
+            Escritor = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+        }
     }
 }
