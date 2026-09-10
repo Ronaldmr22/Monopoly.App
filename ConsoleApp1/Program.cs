4f97a34b7a3e9ce736f83d4909f69607e55d03e9
Alejandro Chaves <chavesfallasa@gmail.com>
2026-09-09 22:38:34 -0600

    Aniade Clase Tablero_casilla_nodo_propiedad

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,224 @@
+﻿namespace Casillas_new { 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Tablero_LL tablerito = new Tablero_LL();
+
+            CasillaEspecial salida= new CasillaEspecial(1,"Salida");
+            Propiedad Casa1 = new Propiedad(2, 1, "Casa 1", 200, 50);
+            Propiedad Casa2 = new Propiedad(3, 2, "Casa 2", 200, 50);
+            Propiedad Casa3 = new Propiedad(4, 3, "Casa 3", 200, 50);
+            Propiedad Casa4 = new Propiedad(5, 4, "Casa 4", 200, 50);
+            Propiedad Casa5 = new Propiedad(6, 5, "Casa 5", 200, 50);
+            CasillaEspecial carcel = new CasillaEspecial(7, "Carcel");
+            Propiedad Casa6 = new Propiedad(8, 6, "Casa 6", 200, 50);
+            CasillaEvento primerevento = new CasillaEvento(9,1);
+            Propiedad Casa7 = new Propiedad(10, 7, "Casa 7", 200, 50);
+            Propiedad Casa8 = new Propiedad(11, 8, "Casa 8", 200, 50);
+            Propiedad Casa9 = new Propiedad(12, 9, "Casa 9", 200, 50);
+            Propiedad Casa10= new Propiedad(13, 10, "Casa 10", 200, 50);
+            CasillaEvento segundoevento = new CasillaEvento(14, 2);
+            Propiedad Casa11 = new Propiedad(15, 11, "Casa 11", 200, 50);
+            Propiedad Casa12 = new Propiedad(16, 12, "Casa 12", 200, 50);
+            Propiedad Casa13 = new Propiedad(17, 13, "Casa 13", 200, 50);
+            CasillaEvento tercerevento = new CasillaEvento(18, 2);
+            Propiedad Casa14 = new Propiedad(19, 14, "Casa 14", 200, 50);
+            Propiedad Casa15 = new Propiedad(20, 15, "Casa 15", 200, 50);
+            Propiedad Casa16 = new Propiedad(21, 16, "Casa 16", 200, 50);
+            CasillaEspecial libre = new CasillaEspecial(22, "Casilla Libre");
+            Propiedad Casa17 = new Propiedad(23, 17, "Casa 17", 200, 50);
+            Propiedad Casa18 = new Propiedad(24, 18, "Casa 18", 200, 50);
+
+
+            tablerito.AgregarCasilla(salida);
+            tablerito.AgregarCasilla(Casa1);
+            tablerito.AgregarCasilla(Casa2);
+            tablerito.AgregarCasilla(Casa3);
+            tablerito.AgregarCasilla(Casa4);
+            tablerito.AgregarCasilla(Casa5);
+            tablerito.AgregarCasilla(carcel);
+            tablerito.AgregarCasilla(Casa6);
+            tablerito.AgregarCasilla(primerevento);
+            tablerito.AgregarCasilla(Casa7);
+            tablerito.AgregarCasilla(Casa8);
+            tablerito.AgregarCasilla(Casa9);
+            tablerito.AgregarCasilla(Casa10);
+            tablerito.AgregarCasilla(segundoevento);
+            tablerito.AgregarCasilla(Casa11);
+            tablerito.AgregarCasilla(Casa12);
+            tablerito.AgregarCasilla(Casa13);
+            tablerito.AgregarCasilla(tercerevento);
+            tablerito.AgregarCasilla(Casa14);
+            tablerito.AgregarCasilla(Casa15);
+            tablerito.AgregarCasilla(Casa16);
+            tablerito.AgregarCasilla(libre);
+            tablerito.AgregarCasilla(Casa17);
+            tablerito.AgregarCasilla(Casa18);
+
+            tablerito.ImprimirTablero();
+
+
+        }
+}
+
+
+public class Casilla
+{
+    protected int numeroCasilla;
+
+    public Casilla(int numeroCasilla)
+    {
+        this.numeroCasilla = numeroCasilla;
+    }
+
+    public int NumeroCasilla
+    {
+        get{ return this.numeroCasilla;}
+    }
+
+    public void Explotar()
+    {
+        Console.WriteLine("La casilla explotó");
+    }
+}
+
+
+class Propiedad : Casilla
+{
+    private int id_propiedad;
+    private String nombre;
+    private double precio;
+    private double alquiler;
+    // Propietario propietario;
+
+    public Propiedad(int numeroCasilla, int id_propiedad, String nombre, double precio, double alquiler)
+    : base(numeroCasilla)
+    {
+        this.id_propiedad = id_propiedad;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.alquiler = alquiler;
+    }
+    public double Precio
+    {
+        get
+        {return this.precio; }
+    }
+    public double Alquiler
+    {
+        get
+        { return this.alquiler;}
+    }
+    public string Nombre
+    {
+        get
+        {return this.nombre;}
+    }
+    public int IdPropiedad
+    {
+        get
+        {return this.id_propiedad;}
+    }
+}
+
+
+class CasillaEspecial : Casilla
+{
+    private string nombre;
+    public CasillaEspecial(int numeroCasilla, String nombre)
+    : base(numeroCasilla)
+    {
+        this.nombre = nombre;
+    }
+
+    public String Nombre
+    {
+        get
+        { return this.nombre;}
+    }
+}
+
+
+class CasillaEvento : Casilla
+{
+    private int idEvento;
+    public CasillaEvento(int numeroCasilla, int idEvento)
+    : base(numeroCasilla)
+    {
+        this.idEvento = idEvento;
+    }
+    public int IdEvento
+    {
+        get
+        {return this.idEvento;}
+    }
+}
+public class Nodo
+{
+    private Casilla data;
+    private Nodo next;
+    private Nodo previous;
+
+    public Nodo(Casilla casilla)
+    {
+        data = casilla;
+        next = null;
+        previous = null;
+    }
+    public Casilla Data
+    {
+        get { return this.data; }
+
+    }
+    public Nodo Next
+    {
+        get { return this.next; }
+        set { this.next = value; }
+    }
+    public Nodo Previous
+    {
+        get { return this.previous; }
+        set { this.previous = value; }
+    }
+
+}
+
+public class Tablero_LL
+{
+    private Nodo head;
+
+
+    public void AgregarCasilla(Casilla casilla)
+    {
+        Nodo nodito = new Nodo(casilla);
+        if (head == null)
+        {
+            nodito.Next = nodito;
+            nodito.Previous = nodito;
+            head = nodito;
+            return;
+        }
+        Nodo tail = head.Previous;
+        nodito.Next = head;
+        nodito.Previous = tail;
+        tail.Next = nodito;
+        head.Previous = nodito;
+
+
+    }
+        public void ImprimirTablero()
+        {
+            if (head == null) return;
+
+            Nodo actual = head;
+            do
+            {
+                // 'actual' es el Nodo, 'actual.Data' es la Casilla, 'actual.Next' es el siguiente Nodo
+                Console.WriteLine($"Casilla: {actual.Data.NumeroCasilla} -> Siguiente Casilla: {actual.Next.Data.NumeroCasilla}");
+                actual = actual.Next;
+            } while (actual != head);
+        }
+    }
+
+}
