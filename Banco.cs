6ef29188d913b553c31812270d427ca89ec8fb7c
leobormen <leo.borbon.mena@gmail.com>
2026-09-10 19:47:56 -0600

    Creación funcion transferir e inicio de destruir jugador

--- Banco.cs
+++ Banco.cs
@@ -1,4 +1,35 @@
 public class Banco
 {
-    
+    public bool Transferir(object Origen, object Destino, int Monto)
+    {
+        if (Origen == this)
+        {
+            Destino.RecibirDinero(Monto);
+            return true;
+        }
+        else if (Destino == this)
+        {
+            if (Origen.PagarDinero(Monto))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+        {
+            if (Origen.PagarDinero(Monto))
+            {
+                Destino.RecibirDinero(Monto);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    public void DestruirJugador(int IdJugador)
+    {
+        
+    }
 }
